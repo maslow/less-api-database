5a8d5ec449ef762fedfcab4dbb5309785183649f
maslow <wangfugen@126.com>
2021-03-20 16:50:55 +0800

    #feat query.merge 支持 交集 选项, intersection;

--- src/query.ts
+++ src/query.ts
@@ -493,8 +493,12 @@ export class Query {
    * 1. 调用 get() 执行主查询
    * 2. 结合主查询的结果，使用 in 执行子表查询
    * 3. 合并主表 & 子表的结果，即聚合
+   * 4. intersection 可指定是否取两个结果集的交集，缺省则以主表结果为主
    */
-  public async merge(options?: { nested: boolean }): Promise<GetRes & ErrorRes> {
+  public async merge(options?: { nested: boolean, intersection: boolean }): Promise<GetRes & ErrorRes> {
+
+    options = options ?? {} as any
+    const intersection = options.intersection ?? false
 
     // 调用 get() 执行主查询
     const res = await this.get(options)
@@ -547,11 +551,20 @@ export class Query {
       }
 
       // 将聚合结果合并入主表结果集中
+      const results = []
       for (let m of res.data) {
         // 此处主表结果中的 [value of `from`] 与 上面子表结果中的 [value of `to`] 应该是一致的
         const key = m[localField]
         m[as] = _map[key]
+
+        // 如果取交集且子表结果无对应数据，则丢弃此条数据
+        if (intersection && !_map[key]) {
+          continue
+        }
+        results.push(m)
       }
+
+      res.data = results
     }
 
     return res
--- tests/merge/with_merge.test.js
+++ tests/merge/with_merge.test.js
@@ -5,7 +5,7 @@ const { Db } = require('../../dist/commonjs/index')
 
 // 这里使用 less-framework 来进行测试，请自行启动 less-framework 服务
 function getAccessToken() {
-  return 'eyJ1aWQiOjEsInJvbGVzIjpbMV0sInR5cGUiOiJhZG1pbiIsImV4cGlyZSI6MTYxNjQ5NzU1ODIxNn0=.6f93ab13ad1066b8eb4b9efbcd346a1a'
+  return 'eyJ1aWQiOiI2MDU1YTJhYWYyODhhNzQyNjEyNjA1MmYiLCJyb2xlcyI6W10sInR5cGUiOiJhZG1pbiIsImV4cGlyZSI6MTYxNjgzMzU0NzY5Mn0=.a5d3f03fbef1c228aef9a31427beef60'
 }
 Db.reqClass = Request
 Db.getAccessToken = getAccessToken
@@ -19,56 +19,40 @@ const db = new Db({
 describe('Db merge', async () => {
 
   it('with & merge', async () => {
-    const res = await db.collection('admin')
-      .with({
-        query: db.collection('user_role').leftJoin('role', 'id', 'role_id'),
+    const res = await db.collection('admins')
+      .withOne({
+        query: db.collection('base_user'),
         localField: 'uid',
-        foreignField: 'uid',
-        as: 'roles'
+        foreignField: '_id',
       })
-      .merge()
+      .merge({ intersection: true })
 
     for (let data of res.data) {
       console.log(data)
     }
   })
 
-  it('withOne & merge', async () => {
-    const res = await db.collection('admin')
-      .withOne({
-        query: db.collection('user_role'),
-        localField: 'uid',
-        foreignField: 'uid',
-        as: 'role'
-      })
-      .merge()
+  // it('withOne & merge (sub query withOne)', async () => {
+  //   const sub_query = db.collection('user_role')
+  //     .field(['uid', 'role_id'])
+  //     .withOne({
+  //       query: db.collection('role'),
+  //       localField: 'role_id',
+  //       foreignField: 'id',
+  //       as: 'info'
+  //     })
 
-    for (let data of res.data) {
-      console.log(data)
-    }
-  })
+  //   const res = await db.collection('admin')
+  //     .withOne({
+  //       query: sub_query,
+  //       localField: 'uid',
+  //       foreignField: 'uid',
+  //       as: 'role',
+  //     })
+  //     .merge()
 
-  it('withOne & merge (sub query withOne)', async () => {
-    const sub_query = db.collection('user_role')
-      .field(['uid', 'role_id'])
-      .withOne({
-        query: db.collection('role'),
-        localField: 'role_id',
-        foreignField: 'id',
-        as: 'info'
-      })
-
-    const res = await db.collection('admin')
-      .withOne({
-        query: sub_query,
-        localField: 'uid',
-        foreignField: 'uid',
-        as: 'role',
-      })
-      .merge()
-
-    for (let data of res.data) {
-      console.log(data)
-    }
-  })
+  //   for (let data of res.data) {
+  //     console.log(data)
+  //   }
+  // })
 })
